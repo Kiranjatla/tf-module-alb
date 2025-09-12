cbe52e41a3aa1f11706bb1d4e65ab38e7234b1c8
KIRAN <ks@gmail.com>
2025-09-12 19:42:00 +0530

    Load Balancers with target groups

--- alb.tf
+++ alb.tf
@@ -45,22 +45,22 @@ resource "aws_security_group" "main" {
   }
 }
 
-#resource "aws_lb_listener" "main" {
-##  count             = var.internal ? 1 : 0
-#  load_balancer_arn = aws_lb.main.arn
-#  port              = "80"
-#  protocol          = "HTTP"
-#
-#  default_action {
-#    type = "fixed-response"
-#
-#    fixed_response {
-#      content_type = "text/plain"
-#      message_body = "Fixed response content"
-#      status_code  = "200"
-#    }
-#  }
-#}
+resource "aws_lb_listener" "main" {
+#  count             = var.internal ? 1 : 0
+  load_balancer_arn = aws_lb.main.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "Fixed response content"
+      status_code  = "200"
+    }
+  }
+}
 
 #
 #resource "aws_lb_listener" "public-http" {
