7e1c9efcb96b40e6b4d6024fec48debebd82e679
KIRAN <ks@gmail.com>
2025-09-12 19:18:55 +0530

    Load Balancers with target groups

--- alb.tf
+++ alb.tf
@@ -21,7 +21,17 @@ resource "aws_security_group" "main" {
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = [var.vpc_cidr]
+#    cidr_blocks = var.internal ? [var.vpc_cidr] : ["0.0.0.0/0"]
   }
+
+#  ingress {
+#    description = "HTTPS"
+#    from_port   = 443
+#    to_port     = 443
+#    protocol    = "tcp"
+#    cidr_blocks = var.internal ? [var.vpc_cidr] : ["0.0.0.0/0"]
+#  }
+
   egress {
     from_port        = 0
     to_port          = 0
@@ -35,19 +45,37 @@ resource "aws_security_group" "main" {
   }
 }
 
-resource "aws_lb_listener" "main" {
-#  count             = var.internal ? 1 : 0
-  load_balancer_arn = aws_lb.main.arn
-  port              = "80"
-  protocol          = "HTTP"
+#resource "aws_lb_listener" "main" {
+##  count             = var.internal ? 1 : 0
+#  load_balancer_arn = aws_lb.main.arn
+#  port              = "80"
+#  protocol          = "HTTP"
+#
+#  default_action {
+#    type = "fixed-response"
+#
+#    fixed_response {
+#      content_type = "text/plain"
+#      message_body = "Fixed response content"
+#      status_code  = "200"
+#    }
+#  }
+#}
 
-  default_action {
-    type = "fixed-response"
-
-    fixed_response {
-      content_type = "text/plain"
-      message_body = "Fixed response content"
-      status_code  = "200"
-    }
-  }
-}
+#
+#resource "aws_lb_listener" "public-http" {
+#  count             = var.internal ? 0 : 1
+#  load_balancer_arn = aws_lb.main.arn
+#  port              = "80"
+#  protocol          = "HTTP"
+#
+#  default_action {
+#    type = "redirect"
+#
+#    redirect {
+#      port        = "443"
+#      protocol    = "HTTPS"
+#      status_code = "HTTP_301"
+#    }
+#  }
+#}
